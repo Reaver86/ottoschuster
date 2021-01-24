b23f8100f6e0c1ad8b1992a4084203faf6099716
Alexander Schuster <alexander.schuster@web.de>
2021-01-24 18:12:24 +0100

    fix ci setup #12

--- apps/frontend/src/app/home/home.component.ts
+++ apps/frontend/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ottos-home',
+  selector: 'otto-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
