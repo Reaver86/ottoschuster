6d427e94e016d513dcfd0aa24cb5522f8782e2bc
Alexander Schuster <alexander.schuster@web.de>
2021-05-15 12:13:42 +0200

    some smaller changes

--- scully.frontend.config.ts
+++ scully.frontend.config.ts
@@ -1,7 +1,17 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { MinifyHtml } from 'scully-plugin-minify-html';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+
+require('./scully/fixStaticLinks');
+require('./scully/sitemapGenerator');
+
+setPluginConfig(baseHrefRewrite, { href: `/${process.env.LOCALE}/` });
+
 export const config: ScullyConfig = {
   projectRoot: './apps/frontend/src',
+  defaultPostRenderers: ['seoHrefOptimise', 'sitemapGenerator', 'fixStaticLinks', baseHrefRewrite, MinifyHtml],
   projectName: 'frontend',
-  outDir: './dist/static',
+  outDir: `./dist/static${process.env.LOCALE}`,
+  distFolder: `./dist/frontend/${process.env.LOCALE}`,
   routes: {},
 };
