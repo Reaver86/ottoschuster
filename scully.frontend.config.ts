dec17b5d5888c909dd49bd0790ed6f96d929ef87
Alexander Schuster <alexander.schuster@web.de>
2021-05-15 14:38:01 +0200

    fix scully config #2

--- scully.frontend.config.ts
+++ scully.frontend.config.ts
@@ -1,9 +1,10 @@
 import { ScullyConfig } from '@scullyio/scully';
 import { MinifyHtml } from 'scully-plugin-minify-html';
+import { criticalCSS } from '@scullyio/scully-plugin-critical-css';
 
 export const config: ScullyConfig = {
   projectRoot: './apps/frontend/src',
-  defaultPostRenderers: ['seoHrefOptimise', MinifyHtml],
+  defaultPostRenderers: ['seoHrefOptimise', criticalCSS, MinifyHtml],
   projectName: 'frontend',
   outDir: './dist/static',
   routes: {},
