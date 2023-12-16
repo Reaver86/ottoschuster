eac5b75271ad95697ad58cab75d376ed62d7d254
alexander.schuster <alexander.schuster86@gmail.com>
2023-12-16 13:38:07 +0100

    fix github pages #11

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://reaver86.github.io/ottoschuster",
-  base: "de",
+  site: "https://reaver86.github.io",
+  base: "/ottoschuster/de",
   i18n: {
     defaultLocale: "de",
     locales: ["de", "ro"],
